71a862bb31d8cf6a55b657c87a50c4a06eae7fc9
Clark Waite <clarkwaite@gmail.com>
2025-05-20 11:25:40 -0400

    Update isBetweenTwoDates to properly handle single day filtering.

--- src/components/shared/helpers.tsx
+++ src/components/shared/helpers.tsx
@@ -1,6 +1,6 @@
 export const isBetweenTwoDates = ({ date, dateFilter }: { date: string, dateFilter: { startDate: string, endDate: string } }): boolean => {
-    const newDate = new Date(date)
-    const startDate = new Date(dateFilter.startDate)
-    const endDate = new Date(dateFilter.endDate)
-    return newDate.valueOf() >= startDate.valueOf() && newDate.valueOf() <= endDate.valueOf()
+    const newDate = new Date(date.split('T')[0]).valueOf()
+    const startDate = new Date(dateFilter.startDate).valueOf()
+    const endDate = new Date(dateFilter.endDate).valueOf()
+    return newDate >= startDate && newDate <= endDate
 }
